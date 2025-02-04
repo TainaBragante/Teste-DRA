fa3d3f15c44a71182b711c968c89a455f05bcc85
TainaBragante <tainacosta543@gmail.com>
2025-02-04 17:27:39 -0300

    DB PostgreSQL

--- meu_app_API/model/__init__.py
+++ meu_app_API/model/__init__.py
@@ -1,30 +1,17 @@
-from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
-
-# importando os elementos definidos no modelo
 from model.base import Base
 from model.funcionario import Funcionario
 
-db_path = "database/"
-# Verifica se o diretorio não existe
-if not os.path.exists(db_path):
-   # então cria o diretorio
-   os.makedirs(db_path)
-
-# url de acesso ao banco (essa é uma url de acesso ao sqlite local)
-db_url = 'sqlite:///%s/db.sqlite3' % db_path
+# url de acesso ao banco 
+db_url = os.getenv("DATABASE_URL","postgresql://flask_db_zyx9_user:G11MaHaLJjCSZYaMTtxqUsSj2jcW0yIY@dpg-cuh6am56147c73be5qn0-a.oregon-postgres.render.com:5432/flask_db_zyx9")
 
 # cria a engine de conexão com o banco
 engine = create_engine(db_url, echo=False)
 
 # Instancia um criador de seção com o banco
 Session = sessionmaker(bind=engine)
-
-# cria o banco se ele não existir 
-if not database_exists(engine.url):
-    create_database(engine.url) 
 
 # cria as tabelas do banco, caso não existam
 Base.metadata.create_all(engine)
